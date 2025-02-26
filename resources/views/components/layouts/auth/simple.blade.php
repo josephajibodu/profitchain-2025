aa01e1610764cdcb9982993becf03e832ba09950
josephajibodu <josephajibodu@gmail.com>
2025-02-26 02:53:33 +0100

    auth fix

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -7,8 +7,8 @@
         <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-md flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-9 mb-6 items-center justify-center rounded-md">
-                        <x-app-logo-icon />
+                    <span class="flex aspect-square size-12 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+                        <x-app-logo-icon class="size-8 fill-current text-white dark:text-black" />
                     </span>
                     <span class="sr-only">{{ config('app.name', 'Profitchain') }}</span>
                 </a>
